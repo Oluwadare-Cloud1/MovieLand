6d433a6f1cb7c63b803c08a84f803d70c27d1c80
Oluwadare-Cloud1 <oluwadareadeneye@gmail.com>
2023-03-10 21:38:10 +0100

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./index.css";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fd4a8338";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=fd4a8338";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
